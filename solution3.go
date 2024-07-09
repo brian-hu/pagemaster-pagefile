35df73d008c93b38f66a78f50ecbdbfc20e2c4bb
bhu143 <bhu143@bloomberg.net>
2024-07-09 11:20:34 -0500

    more testing

---
+++ solution3.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func SearchString(mem *Memory) (string, error) {
+    b, err := mem.ReadAddress(0)
+    if err != nil {
+        return "", err
+    }
+    fmt.Println(b)
+    b, err = mem.ReadAddress(12312442)
+    if err != nil {
+        return "", err
+    }
+    fmt.Println(b)
+
+    bs, err := mem.ReadPage(0)
+    if err != nil {
+        return "", err
+    }
+    fmt.Println(string(bs))
+
+    bs, err = mem.ReadPage(1000000)
+    if err != nil {
+        return "", err
+    }
+    fmt.Println(string(bs))
+	return "", nil
+}
