1b4dad9decb81eab51b4fad633a4dd917893f0d0
bhu143 <bhu143@bloomberg.net>
2024-07-09 15:12:54 -0500

    more testing

---
+++ solution8.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf16"
+)
+
+func SearchString(mem *Memory) (string, error) {
+	memAddress := 0
+	var window []byte
+	for {
+		b, err := mem.ReadAddress(memAddress)
+		if err != nil {
+			return "", err
+		}
+		fmt.Print(string(b))
+
+		if len(window) < 5 {
+			window = append(window, b)
+		} else {
+			if string(window) == "gc24{" {
+				fmt.Println("here")
+				break
+			} else {
+				window = append(window, b)[1:]
+			}
+		}
+
+		memAddress++
+	}
+
+	return "", nil
+}
